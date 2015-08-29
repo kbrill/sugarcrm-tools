8a26d6a33335927a9d39a1d299d428dab78e6f34
Ken Brill <kbrill@sugarcrm.com>
2015-08-29 15:47:27 -0500

    A script to automatically generate the installer ZIP files

---
+++ mkInstallers.php
@@ -0,0 +1,38 @@
+<?php
+
+$packageArray = array('fixForecasts', 'fixLanguageFiles');
+
+foreach ($packageArray as $packageName) {
+    // Get real path for our folder
+    $rootPath = realpath($packageName);
+    echo "Working on {$rootPath}\n";
+    // Initialize archive object
+    $zip = new ZipArchive();
+    $zip->open($packageName.'.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+    // Initialize empty "delete list"
+    $filesToDelete = array();
+
+    // Create recursive directory iterator
+    /** @var SplFileInfo[] $files */
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($rootPath),
+        RecursiveIteratorIterator::LEAVES_ONLY
+    );
+
+    foreach ($files as $name => $file) {
+        // Skip directories (they would be added automatically)
+        if (!$file->isDir()) {
+            // Get real and relative path for current file
+            $filePath = $file->getRealPath();
+            $relativePath = substr($filePath, strlen($rootPath) + 1);
+            echo "Adding '$filePath'\n";
+            // Add current file to archive
+            $zip->addFile($filePath, $relativePath);
+        }
+    }
+
+    // Zip archive will be created only after closing object
+    $zip->close();
+
+}
